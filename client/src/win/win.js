4212c0533bab782464a6bf5ba6ef11f35865ccfc
chwoodv <148597237+chwoodv@users.noreply.github.com>
2023-10-20 21:18:25 -0500

    Updated

--- client/src/win/win.js
+++ client/src/win/win.js
@@ -1,5 +1,13 @@
+
+
 export default function Win(){
+    var test = [];
+    for (var i = 0; i < 10; i++) {
+        test.push(<div><p>{i}</p></div>)
+    }
     return (<div className="winScreen">
         <h2>Win</h2>
+        {test}
+        <image></image>
     </div>)
 }
