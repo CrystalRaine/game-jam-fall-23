9d1204f03d00b57a5bdb0e0068e949fe2e66889d
EclipsesShadow <bencol1022@gmail.com>
2023-10-21 17:12:32 -0500

    advanced attacks

--- client/src/level/level.js
+++ client/src/level/level.js
@@ -10,6 +10,9 @@ export default function Level({gameWS, setGameWS, username}){
     const [p2y, setp2y] = useState(0);
     const [p1health, setp1health] = useState(0);
     const [p2health, setp2health] = useState(0);
+    
+    const [p1name, setp1name] = useState("");
+    const [p2name, setp2name] = useState("");
 
     async function move(x, y){
         gameWS.send(JSON.stringify({type:"input", username:username, posX:x, posY:y}));
@@ -19,22 +22,22 @@ export default function Level({gameWS, setGameWS, username}){
     }
 
     const handler = (event) => {
-        if (event.key === 'w') {
+        if (event == 87 || event == 32) {
             move(0, -7);
         }
-        else if(event.key === 'a') {
+        else if(event == 65) {
             move(-2, 0);
         }
-        else if(event.key === 's') {
+        else if(event == 83) {
             move(0, 50);
         }
-        else if(event.key === 'd') {
+        else if(event == 68) {
             move(2, 0);
         }
-        else if(event.key === 37) {
+        else if(event == 37) {
             attack(-1)
         }
-        else if(event.key === 39) {
+        else if(event == 39) {
             attack(1);
         }
     }
@@ -47,13 +50,24 @@ export default function Level({gameWS, setGameWS, username}){
             setp2y(data.p2.position.y);
             setp1x(data.p1.position.x);
             setp1y(data.p1.position.y);
+
+            setp1health(data.p1.health);
+            setp2health(data.p2.health);
+
+            setp1name(data.p1.username);
+            setp2name(data.p2.username);
         };
+
+        document.addEventListener('keydown', function(event){
+            // console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed`);
+            handler(event.keyCode);
+        });   
     }, []);
 
-    return (<div className="levelScreen" onKeyDown={handler}>
+    return (<div className="levelScreen">
+    <p>{p1name}: {p1health} | {p2name}: {p2health}</p>
         <div id="battlefield"></div>
         <div id="player1" className="player" style={{position: "absolute", left:p1x + 'px', top:p1y + 'px'}}>Player1</div>
         <div id="player2" className="player" style={{position: "absolute", left:p2x + 'px', top:p2y + 'px'}}>Player2</div>
-        <button autoFocus className="focusButton"></button>
     </div>)
 }
