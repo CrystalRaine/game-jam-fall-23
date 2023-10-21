0d2b75f2099d4baf3e08221ad83f84901f3ec793
TheShauceGod <bdiaz9@iastate.edu>
2023-10-21 17:39:18 -0500

    Rick Roll

--- client/src/lose/lose.js
+++ client/src/lose/lose.js
@@ -6,9 +6,10 @@ export default function Lose(){
     function playMusic() {
         new Audio(consolation).play();
     }
-    playMusic()
+    //playMusic()
 
     return (<div className="loseScreen">
         <h2>Lose</h2>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Strategy Guide</a>
     </div>)
 }
