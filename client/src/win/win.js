d3ad529c43461378cc68aca9e556497840336cc8
TheShauceGod <bdiaz9@iastate.edu>
2023-10-22 09:47:35 -0500

    ?

--- client/src/win/win.js
+++ client/src/win/win.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import picture from './Graph.png';
 
 export default function Win(){
     var test = [];
-    for (var i = 0; i < 3; i++) {
-        test.push(<div><p>{i}</p></div>)
-    }
     return (<div className="winScreen">
         <h2>Win</h2>
-        {test}
-        <img src={picture} alt="Picture" />
     </div>)
 }
